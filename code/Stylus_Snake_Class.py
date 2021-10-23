fbf3cac8f3822410b98f808ee305337c0c2233ec
Aditya Jivoji <89838921+adityajivoji@users.noreply.github.com>
2021-10-23 15:11:54 +0530

    Update Stylus_Snake_Class.py

--- code/Stylus_Snake_Class.py
+++ code/Stylus_Snake_Class.py
@@ -347,7 +347,7 @@ while run_condition:
             run_condition = False
 
         # Frame rate
-        clock.tick(13)
+        clock.tick(30)
 
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
